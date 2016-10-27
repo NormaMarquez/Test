9a6fe07c0e5d86bb4676e9577f51842c40e8ba2a
NormaMarquez <neca_marquez@hotmail.com>
2016-10-26 19:07:12 -0500

    Versión3: Página Principal (Panel Izquierdo)

---
+++ PanelIzquierdo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<HTML lang="en-ES">
+
+		<HEAD>
+			<title>Panel Izquierdo</title>
+			<meta charset="UTF-8">
+			
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</HEAD>
+		<BODY bgcolor="grey">
+
+			<frame me name="alto" src="index1.php">  			 
+			<div class="container" id="menus"> 
+			   <ul class="nav nav-pills nav-stacked">
+						<li><a href="ini.php" target="central">INICIO</a></li>
+									
+						<li><a href="xxxxx" target="central">OBJETIVOS</a></li>
+						
+						<li><a href="xxxxx" target="central">JUSTIFICACIÓN</a></li>
+					
+						<li><a href="xxxxx" target="central">METODOLOGÍA</a></li>
+	
+						<li><a href="xxxxx" target="central"> SQA</a></li>
+						
+						<li><a href="xxxxx" target="central"> DIAGRAMAS</a></li>
+
+						<li><a href="xxxxx" target="central"> PDF</a></li>
+						
+			  </ul>
+			</div>
+		</BODY>
+</HTML>
