968509cf76cc10ea0821eba2c94911c55b144d91
NormaMarquez <neca_marquez@hotmail.com>
2016-10-26 19:04:23 -0500

    Versión1: Página Principal

---
+++ index1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<HTML lang="en-ES">
+
+		<HEAD>
+		
+				
+			<TITLE>PROYECTO INGENIERIA DE SOFTWARE</TITLE>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" type="text/css" href="style.css">
+			<BASE TARGET="principal">
+			
+		</HEAD>
+		
+		<FRAMESET ROWS=18%,* >
+			
+			 <FRAME name="superior" SRC="PanelSuperior.php" scrolling="no" noresize>
+				  <FRAMESET COLS=17%,80%,17%* border=0 >
+					 <FRAME SRC="PanelIzquierdo.php" NAME="izquierdo" scrolling="no" noresize>
+					 <FRAME SRC="PanelCentral.php" NAME="central" scrolling="y" noresize>  
+					 <FRAME SRC="PanelDerecho.php" NAME="derecho" scrolling="no" noresize>  
+				  </FRAMESET>
+				
+		</FRAMESET>
+	
+		
+
+
+     
+	</HTML>
