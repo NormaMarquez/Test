cb0dd33417d6358de7c5bc3d092b49f1f7625d69
NormaMarquez <neca_marquez@hotmail.com>
2016-10-26 19:07:42 -0500

    Versión4: Página Principal (Panel Derecho)

---
+++ PanelDerecho.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<HTML lang="en-ES">
+
+	<HEAD>
+		<title>Panel Derecho</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	 </HEAD>
+
+	<BODY bgcolor="grey">
+
+		 <frame me name="alto" src="index1.php">  
+		 
+		 
+		 
+		<div class="container" id="menus"> 
+				<ul class="nav nav-pills nav-stacked">
+			
+					<li><a href="xxxx" target="central">REGISTRO ANALISTAS</a></li>
+					<li><a href="xxxx" target="central">AUDITORIAS</a></li>
+					<li><a href="xxxx" target="central">ESTADISTICAS</a></li>
+					<li><a href="xxxxx" target="central">OTROS</a></li>
+				
+				</ul>
+		</div>
+		
+	</BODY>
+</HTML>
