5081aad6bcb597f8c3be72cab31a848243227e48
NormaMarquez <neca_marquez@hotmail.com>
2016-10-26 19:06:31 -0500

    Versión2: Página Principal (Panel Superior)

---
+++ PanelSuperior.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<HTML lang="en-ES">
+
+	<HEAD>
+		<TITLE>Panel Superior</TITLE>
+		<meta charset="UTF-8">
+		
+	</HEAD>
+	<BODY bgcolor="grey">
+		<center>
+			<marquee behavior="alternate"> <br />
+			<img height="90" width="1000" alt="" src="tashett Testing.PNG"/>
+						
+			 <frame me name="alto" src="index1.php">  
+			 </marquee> <br />
+		</center>
+			 
+	</BODY>
+</HTML>
