73a3995f4888102c364ef04213e0a165d6cd41e4
NormaMarquez <neca_marquez@hotmail.com>
2016-10-26 19:28:25 -0500

    Versión5: Página Principal (Panel Central)

---
+++ PanelCentral.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<HTML lang="en-ES">
+
+	<HEAD>
+		<title>Panel Central</title>
+		<meta charset="UTF-8">
+		
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</HEAD>
+	
+	<BODY bgcolor="grey">
+			
+			<ins><h2 style="color:black"  font face="forte" align="center"><abbr title="Es un satelite">DESCRIPCIÓN:</abbr>PROYECTO</font></style></h2>
+			</ins>
+			<hr>
+			<h4><a href="Imagen-Calidad de software.png" target="_blank">Ver imagen: Calidad de Software</a></h4>
+			<h4><a href="Imagen-Factores Claves de Calidad del Servicio.jpg" target="_blank">Ver imagen: Factores Claves de Calidad del Servicio</a></h4>
+			
+			<div align="left">	
+			<a style="color:white"> Desarrollar una aplicación que va dirigida a empresas y/o outsourcing, con el objetivo de ofrecer un rápido y sencillo acceso a resultados de evaluación de cada uno de sus empleados y operarios de una mesa de ayuda. La aplicación funciona sobre cualquier sistema operativo que cuente con acceso a internet.<br><br>
+			Se diseñará un sistema de fácil acceso y de forma rápida a visualizar los resultados por personas, por días, por semanas y por meses.<br><br> Considerando necesario el desarrollo de un nuevo sistema de gestión de auditorías, así como las bases de datos que recogen datos tanto estadísticos y empresariales del personal, por tanto los resultados de estas auditorías tendrán una gestión más rápida, automática y segura.</a></p>
+			<h4><a href="https://calidadtotaltqm.wikispaces.com/Aplicaciones+del+control+de+la+calidad+total"  target="_blank">De clik aquí para visualizar: Referencias de PHP </a></h4>
+			<div>
+			
+		
+	
+					
+	</BODY>
+</HTML>
