80368a25a15ab93e8e50fcf24e3e4918da4cc6f5
NormaMarquez <neca_marquez@hotmail.com>
2016-11-15 14:58:04 -0500

    Versión7: Página completa

---
+++ controlversiones.php
@@ -0,0 +1,52 @@
+ 	<!DOCTYPE html>
+<html lang="en-ES">
+<head>
+			<meta charset="UTF-8">
+			
+			
+			<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<BODY bgcolor="grey">
+<br/>
+<br>
+
+<ins><h2 style="color:black; font family="forte" align="center">CONTROL DE VERSIONES</h2><br></ins>
+
+				<div class="container">
+				<iframe src="http://www.desarrolloweb.com/articulos/introduccion-git-github.html" width="800" height="400" style="float:center"></iframe>
+				</div>
+<hr>
+<br><br>
+ 
+ 
+ <ins><h2 style="color:black; font family="forte">SOBRE NUESTRO PROYECTO</h2><br></ins>
+ 
+ 
+				<h4><a href="https://github.com/NormaMarquez/Test"  target="_blank">Ir a GitHub </a></h4>
+				
+ 
+ <br>
+ <hr>
+
+
+
+<!--<ins><h3 style="color:black"  font family="forte" align="center">MODELO ENTIDAD RELACIÓN</h3><hr></ins>
+<center>
+			<!--<marquee behavior="alternate"> <br />
+			<img height="300" width="700" alt="" src="Modeloentidadrelacion.jpg"/>
+<ins><h3 style="color:black"  font family="forte" align="center">MODELO RELACIONAL</h3><hr></ins>
+			<img height="300" width="700" alt="" src="Modelorelacional.jpg"/>
+<ins><h3 style="color:black"  font family="forte" align="center">UN CASO DE USO</h3><hr></ins>
+			<img height="300" width="700" alt="" src="casodeuso.jpg"/>
+
+</center>
+		
+		<center>
+			<marquee behavior="alternate"> <br />
+			<img height="100" width="100" alt="" src="logo.jpg"/>
+			</marquee> <br />
+</center>-->
+
+<input type="button" onclick="location='PanelCentral.php'" value="Regresar al inicio"/></h5>
+</body>
+</HTML>
