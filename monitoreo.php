d834e8e3d41a7fd3ae3c8ebcc5003f820d7f6ad0
NormaMarquez <neca_marquez@hotmail.com>
2016-11-15 14:59:15 -0500

    Versión7: Página completa

---
+++ monitoreo.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en-ES">
+<head>
+			<meta charset="UTF-8">
+			
+			<link rel="stylesheet" type="text/css" href="style.css">
+
+			<title>Monitoreo</title>
+
+</head>
+<BODY bgcolor="20706D">
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg .tg-o13c{background-color:#4174c7;text-align:center;vertical-align:top}
+.tg .tg-yw4l{vertical-align:top}
+.tg .tg-1h4a{background-color:#5cac44;text-align:center;vertical-align:top}
+.tg .tg-8eaw{background-color:#5cac44;vertical-align:top}
+</style>
+<center>
+<table class="tg">
+  <tr>
+    <th class="tg-o13c" colspan="8">PLANTILLA DE MONITOREO<br></th>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Nombre:</td>
+    <td class="tg-yw4l" colspan="3"><input type='text' name='a' /></td>
+    <td class="tg-yw4l">Fecha Monitoreo:</td>
+    <td class="tg-yw4l" colspan="3"><input type='text' name='b' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Fecha Llamada:</td>
+    <td class="tg-yw4l" colspan="3"><input type='text' name='c' /></td>
+    <td class="tg-yw4l">Nota:</td>
+    <td class="tg-yw4l" colspan="3"><input type='text' disabled="disable" name='d' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">ID Asesor:<br></td>
+    <td class="tg-yw4l" colspan="3"><input type='text' name='e' /></td>
+    <td class="tg-yw4l">ID Llamada:</td>
+    <td class="tg-yw4l" colspan="3"><input type='text' name='f' /></td>
+  </tr>
+  <tr>
+    <td class="tg-o13c" colspan="4">ITEMS</td>
+    <td class="tg-o13c" colspan="4">CALIFICACIÓN (1 A 10)</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">1. Saludo</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='g' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">2.Validacion de datos</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='h' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">3.Cubre silencios durante las busquedas<br></td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='i' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">4.Orden de la herramienta</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='j' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">5.Sigue todo el proceso establecido</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='k' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">6.Puede dar solucion al inconveniente</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='l' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">7.Utiliza vocabulario adecuado</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='m' /></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" colspan="4">8.Despedida</td>
+    <td class="tg-yw4l" colspan="4"><input type='text' name='n' /></td>
+  </tr>
+  <tr>
+    <td class="tg-1h4a" colspan="4">TOTAL</td>
+    <td class="tg-8eaw" colspan="4"><input type='text' disabled="disable" name='o' /></td>
+  </tr>
+</table>
+</center>
+<br></br>
+<center></a><input type='submit' value='Guardar' /><br><br></center>
+<center>
+			<marquee behavior="alternate"> <br />
+			<img height="100" width="100" alt="" src="logo.jpg"/>
+			</marquee> <br />
+</center>
+
+
+<input type="button" onclick="location='PanelCentral.php'" value="Regresar al inicio"/></h5>
+</body>
+</html>
+
